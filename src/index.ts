fb23377ef3e431f319d56e5047ea9ceec19d2643
ROBOT <tkata12@outlook.jp>
2026-04-13 00:54:17 +0900

    add: discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import "dotenv/config";
+
+import { Client, GatewayIntentBits } from "discord.js";
+
+const token = process.env.TOKEN;
+
+if (!token) {
+  throw new Error("Token not found.");
+}
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+await client.login(token);
