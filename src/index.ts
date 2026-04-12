7a6bc0ef4b82a6a93a269b6b7c333521ef777ae2
ROBOT <tkata12@outlook.jp>
2026-04-13 01:32:52 +0900

    fix: add schedule

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import "dotenv/config";
 
-import { Client, GatewayIntentBits } from "discord.js";
+import { ChannelType, Client, EmbedBuilder, GatewayIntentBits } from "discord.js";
+import { schedule } from "node-cron";
 
 const token = process.env.TOKEN;
+const noticeChannelId = process.env.NOTICE_CHANNEL;
+const targetCategories = process.env.TARGET_CATEGORIES;
+const roleId = process.env.ROLE_ID;
 
-if (!token) {
-  throw new Error("Token not found.");
+if (!token || !noticeChannelId || !targetCategories || !roleId) {
+  throw new Error("Invalid .env");
 }
+
+process.on("uncaughtException", (error) => {
+  console.error(error);
+});
+
+process.on("unhandledRejection", (error) => {
+  console.error(error);
+});
 
 const client = new Client({
   intents: [
@@ -19,5 +31,64 @@ const client = new Client({
     GatewayIntentBits.MessageContent
   ]
 });
+
+schedule(
+  "* 30 19 * * 1-5",
+  async () => {
+    const targetCategoryIds = targetCategories.split(",");
+    const channels = client.channels.cache.values();
+    const noticeChannel = client.channels.cache.get(noticeChannelId);
+
+    for (const channel of channels) {
+      if (channel.type === ChannelType.GuildCategory && targetCategoryIds.includes(channel.id)) {
+        await channel.permissionOverwrites.create(roleId, { ViewChannel: true });
+      }
+    }
+
+    if (noticeChannel?.type === ChannelType.GuildText) {
+      const embed = new EmbedBuilder()
+        .setTitle("開会のお知らせ")
+        .setDescription(
+          "予定通り開会いたします。本日も萬游會をご利用いただきありがとうございます。"
+        );
+      await noticeChannel.send({
+        content: `<@&${roleId}>`,
+        embeds: [embed]
+      });
+    }
+  },
+  {
+    timezone: "Asia/Tokyo"
+  }
+);
+
+schedule(
+  "* * 5 * * 1-5",
+  async () => {
+    const targetCategoryIds = targetCategories.split(",");
+    const channels = client.channels.cache.values();
+    const noticeChannel = client.channels.cache.get(noticeChannelId);
+
+    for (const channel of channels) {
+      if (channel.type === ChannelType.GuildCategory && targetCategoryIds.includes(channel.id)) {
+        await channel.permissionOverwrites.create(roleId, { ViewChannel: false });
+      }
+    }
+
+    if (noticeChannel?.type === ChannelType.GuildText) {
+      const embed = new EmbedBuilder()
+        .setTitle("閉会のお知らせ")
+        .setDescription(
+          "本日はこれにて閉会です。開会は本日19:30となります。ご利用いただきありがとうございます。"
+        );
+      await noticeChannel.send({
+        embeds: [embed]
+      });
+    }
+  },
+  {
+    timezone: "Asia/Tokyo"
+  }
+);
 
 await client.login(token);
